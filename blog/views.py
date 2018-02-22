68c539b3bdc224ec250e8230c9c8621b2ef1be00
scallopy <scallopy@abv.bg>
2018-02-22 11:02:48 +0200

    All files

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+import datetime
+
+def index(request):
+    return HttpResponse("Hello Django")
+
+def today_is(request):
+    now = datetime.datetime.now()
+    html = "<html><body>Current date and time: {0}</body></html>".format(now)
+    return HttpResponse(html)
+
+
