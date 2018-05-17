f70d4bf31166ef6cc61b4a1e2e11510be01aec45
scallopy <scallopy@abv.bg>
2018-05-17 11:02:35 +0300

    static

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 MODELTRANSLATION_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['autoelectronicselectra.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'cadmin',
     'mptt',
     'flatpages_i18n',
-    'openinghours',
 ]
 
 MIDDLEWARE = [
@@ -149,9 +148,8 @@ DBGETTEXT_PROJECT_OPTIONS = 'django_project.dbgettext_options'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
